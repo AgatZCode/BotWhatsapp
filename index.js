87ce67dd1f40a0acd6fad02b9bbe397d4da97c05
AgatZ <98450255+AgatZCode@users.noreply.github.com>
2023-01-09 23:42:30 +0700

    Update index.js

--- index.js
+++ index.js
@@ -24,8 +24,22 @@ client.on('message', async msg => {
     const text = msg.body.toLowerCase() || '';
 
     //check status
-    if (text === '!ping') {
-        msg.reply('pong');
+   if (msg.body == 'ping') {
+        const startTime = Date.now();
+        await msg.reply('Sedang menghitung kecepatan bot...');
+        const endTime = Date.now();
+        const timeDifference = endTime - startTime;
+        await msg.reply(`Ping Bot ${timeDifference} milidetik.`);
+    }
+    //salam
+    if (text === 'assalamualaikum') {
+        msg.reply('waalaikumsalam');
+    }
+    if (text === 'hai') {
+        msg.reply('hai jugaaa! ,untuk bertanya gunakan .tanya dan buat sebuah pertanyaan yang ingin anda tanyakan');
+    }
+    if (text === 'saya ingin bertanya') {
+        msg.reply('silahkan tanyakan apa yang ingin anda tanyakan , gunakan .tanya apa yang inginanda tanyakan ,akan di jawab sesuai yang anda ketik dengan tepat danjawaban juga akan tepat');
     }
 
     // edit_bg/bg_color
