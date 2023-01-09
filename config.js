b2e3b52027a9bde6d43bd191dfb2a5e5d48b3337
AgatZ <98450255+AgatZCode@users.noreply.github.com>
2023-01-09 23:46:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    API_KEY_RM_BG: "YOUR_API_KEY",
-    API_KEY_OPEN_AI: "YOUR_API_KEY"
+    API_KEY_RM_BG: "tjSJKjm64tpAzxwxiCzUhc22",
+    API_KEY_OPEN_AI: "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"
 }
